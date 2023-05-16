58128a8589a3f1fe06fc30985e4636c5264162ec
javia <javelr00@estudiante.unileon.es>
2023-05-16 14:03:01 +0200

    Cambio 3

--- src/Controlador.java
+++ src/Controlador.java
@@ -11,6 +11,9 @@ public class Controlador implements ActionListener {
     private Cliente cliente;
     private File transferencia;
     private File fichDescarga;
+    ObjectInputStream entradaCliente;
+    ObjectOutputStream salidaCliente;
+    PrintWriter salidaClientePrint;
     private Socket socket;
     private List<Transferencia> lista;
     private DefaultListModel<String> listModel;
@@ -28,6 +31,7 @@ public class Controlador implements ActionListener {
     private void vincularCliente(ActionListener listener){
         cliente.getbConectar().addActionListener(listener);
         cliente.getbSubir().addActionListener(listener);
+        cliente.getbDescargar().addActionListener(listener);
     }
 
     private void vincularServidor(ActionListener listener){
@@ -39,71 +43,152 @@ public class Controlador implements ActionListener {
         String elige=e.getActionCommand();
         switch (elige){
             case "Iniciar":
+                SwingWorker<List<Transferencia>, Void> worker = new SwingWorker<List<Transferencia>, Void>() {
+                    @Override
+                    protected List<Transferencia> doInBackground() throws Exception {
+                        try {
+                            ServerSocket serverSocket = new ServerSocket(55555);
+                            do {
+                                System.out.println("Se inicia el servidor");
+                                socket= serverSocket.accept();
+                                HiloCliente hiloCliente = new HiloCliente(socket);
+                                System.out.println("Cliente conectado");
+                                hiloCliente.start();
+                            }while (!serverSocket.isClosed());
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
 
-                try {
-                    ServerSocket serverSocket = new ServerSocket(55555);
-                    do {
-                        System.out.println("Se inicia el servidor");
-                        socket= serverSocket.accept();
-                        HiloCliente hiloCliente = new HiloCliente(socket);
-                        System.out.println("Cliente conectado");
-                    }while (!serverSocket.isClosed());
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
+                        //ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
+                        //ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
+
+                        //lista = (List<Transferencia>) entrada.readObject();
+
+                        //return lista;
+                        return null;
+                    }
+
+                    @Override
+                    protected void done() {
+                        try {
+                            for(int i=0; i < lista.size(); i++){
+                                DefaultListModel modelo = new DefaultListModel();
+                                cliente.getList1().setModel(listModel);
+                                modelo.addElement(lista.get(i).getNombre());
+                                cliente.getList1().setModel(modelo);
+                            }
+
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                };
+                worker.execute();
                 break;
             case "Conectar":
-                //Hilo infinito de espera de escucha a clientes
+                Thread hilo = new Thread(() -> {
+                    try {
+                        //Leer ip de la gui
+                        System.out.println(cliente.gettextfIp().getText());
+                        Socket socket = new Socket(cliente.gettextfIp().getText(),55555);
+                        //Si hacemos esto puede que no nos aseguremos de que realmente esté creado el input y outputstream
+                        cliente.getbSubir().setEnabled(true);
+                        cliente.getbDescargar().setEnabled(true);
+                        //entradaCliente = new ObjectInputStream(socket.getInputStream());
+                        salidaCliente = new ObjectOutputStream(socket.getOutputStream());
+                        try {
+                            salidaClientePrint = new PrintWriter(socket.getOutputStream(), true);
+                            salidaClientePrint.println("hola servidor");
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        //socket.close();
 
-                //Cuando un cliente se conecta, crear un hilo, que se quede a la espera de recibir 3 opciones
-                //-Obtener lista -Subir fichero -Descargar fichero
 
-                //Crear hilo
-                HilosCliente hilo = new HilosCliente();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                });
                 hilo.start();
-
-                try {
-                    //Leer ip de la gui
-                    System.out.println(cliente.gettextfIp().getText());
-                    Socket socket = new Socket(cliente.gettextfIp().getText(),55555);
-//                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
-//                    printWriter.println("Lista de elementos");
-//
-//                    String respuesta = bufferedReader.readLine();
-//                    System.out.println("Lista de elementos"+respuesta);
-                    cliente.getbSubir().setEnabled(true);
-                    cliente.getbDescargar().setEnabled(true);
-                    socket.close();
-
-
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
                 break;
+
             case "Subir":
+                salidaClientePrint.println("Subir");
                 JFileChooser selecciona = new JFileChooser();
+
                 int accion = selecciona.showSaveDialog(null);
                 if (accion == JFileChooser.APPROVE_OPTION) {
+                    //Crear hilo swingworker que notifique que va a enviar un fichero
                     transferencia = selecciona.getSelectedFile();
+                    try {
+                        FileInputStream lectorFichero = new FileInputStream(transferencia);
+                        salidaCliente.writeLong(transferencia.length());
+
+                        byte[] buffer = new byte[1024];
+                        int bytesLeidos = 0;
+                        while( (bytesLeidos = lectorFichero.read(buffer)) > 0){
+                            //Me aseguro de que se escriben todos lo bytes del buffer
+                            salidaCliente.flush();
+                            //Envio datos a través del socket
+                            salidaCliente.write(buffer, 0, bytesLeidos);
+
+                        }
+                        lectorFichero.close();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 }
+
                 break;
 
             case "Descargar":
-                JFileChooser descarga = new JFileChooser();
+                salidaClientePrint.println("Descargar");
+                System.out.println("pulsado boton descargar");
+                salidaClientePrint.println("hola servidor desde descargas");
+
+                /*JFileChooser descarga = new JFileChooser();
                 int evt = descarga.showSaveDialog(null);
                 if (evt == JFileChooser.APPROVE_OPTION) {
-                    fichDescarga = descarga.getSelectedFile();
-                }
-            case "Refrescar":
+                    SwingWorker<List<Transferencia>, Void> workerSubir = new SwingWorker<List<Transferencia>, Void>() {
+                        @Override
+                        protected List<Transferencia> doInBackground() throws Exception {
+                            fichDescarga = descarga.getSelectedFile();
 
-                SwingWorker<List<Transferencia>, Void> worker = new SwingWorker<List<Transferencia>, Void>() {
+                            return lista;
+                        }
+
+                        @Override
+                        protected void done() {
+                            try {
+                                for(int i=0; i < lista.size(); i++){
+                                    DefaultListModel modelo = new DefaultListModel();
+                                    cliente.getList1().setModel(listModel);
+                                    modelo.addElement(lista.get(i).getNombre());
+                                    cliente.getList1().setModel(modelo);
+                                }
+
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    };
+
+                }*/
+                break;
+            case "Refrescar":
+                salidaClientePrint.println("Refrescar");
+                SwingWorker<List<Transferencia>, Void> worker2 = new SwingWorker<List<Transferencia>, Void>() {
                     @Override
                     protected List<Transferencia> doInBackground() throws Exception {
                         //Buscar en el servidor los archivos disponibles
                         ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
                         ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
-
+                        //                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+//                    printWriter.println("Lista de elementos");
+//
+//                    String respuesta = bufferedReader.readLine();
+//                    System.out.println("Lista de elementos"+respuesta);
                         lista = (List<Transferencia>) entrada.readObject();
 
                         return lista;
--- src/HiloCliente.java
+++ src/HiloCliente.java
@@ -1,9 +1,71 @@
+import java.io.*;
 import java.net.Socket;
 
 public class HiloCliente extends Thread {
     private Socket cliente;
+    private ObjectInputStream entradaCliente;
+    private ObjectOutputStream salidaCliente;
+    private BufferedReader entrada;
 
     public HiloCliente(Socket cliente) {
         this.cliente = cliente;
     }
+    @Override
+    public void run() {
+
+        try {
+            entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+            while (true) {
+                String mensajeRecibido = entrada.readLine();
+                if(mensajeRecibido != null) {
+                    switch(mensajeRecibido) {
+                        case "Subir":
+                            try {
+                                File ficheroDestino = new File("C:\\Users\\javia\\Documents\\documento3");
+                                FileOutputStream escritorFichero = new FileOutputStream(ficheroDestino);
+
+                                this.entradaCliente = new ObjectInputStream(cliente.getInputStream());
+
+                                long fileSize = entradaCliente.readLong();
+
+                                byte[] buffer = new byte[1024];
+                                int bytesLeidos;
+                                long totalLeido = 0;
+                                while( totalLeido < fileSize && (bytesLeidos = entradaCliente.read(buffer)) > 0 ){
+                                    escritorFichero.write(buffer, 0, bytesLeidos);
+                                    totalLeido += bytesLeidos;
+                                }
+                                escritorFichero.close();
+
+                                System.out.println(ficheroDestino.getName());
+
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            break;
+                        case "Descargar":
+                            try {
+                                this.salidaCliente = new ObjectOutputStream(cliente.getOutputStream());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            break;
+                        case "Refrescar":
+                            try {
+                                this.salidaCliente = new ObjectOutputStream(cliente.getOutputStream());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                            break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
